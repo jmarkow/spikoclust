53bdd8b2e56709b8941dd334fcd959664d9bf31a
Jeff Markowitz <markowitzmeister@gmail.com>
2015-04-02 12:13:16 -0400

    Forcing symmetry fix of covariance by default

--- helpers/spikoclust_gmem_covcheck.m
+++ helpers/spikoclust_gmem_covcheck.m
@@ -3,12 +3,5 @@ function SIGMA=cov_check(SIGMA)
 [~,~,NCLUST]=size(SIGMA);
 
 for i=1:NCLUST
-	p=spikoclust_gmem_ispdm(SIGMA(:,:,i));
-
-	if ~p
-		disp('Fixing covariance...');
-		SIGMA(:,:,i)=spikoclust_gmem_covfix(SIGMA(:,:,i));
-	end
-end
-
+	SIGMA(:,:,i)=spikoclust_gmem_covfix(SIGMA(:,:,i));
 end
